63777d3216f2a703906f961c3bfe8a861944f2c3
karan <karanche97@gmail.com>
2024-12-02 00:10:07 +0530

    [config] todo controller template

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { resolve } = require("path");
 require("dotenv").config();
-const todoController = require("./controllers/todo.controller");
+const todoController = require("./todo.controller");
 
 const app = express();
 const port = process.env.PORT || 3001;
